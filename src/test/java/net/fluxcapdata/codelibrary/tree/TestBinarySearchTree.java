39a0b4bcdfc56f2777fe1b83838bafa7cd9b3d4d
johns <johnscheibmeir@gmail.com>
2024-08-26 21:16:46 -0700

    new test case to improve coverage

--- src/test/java/net/fluxcapdata/codelibrary/tree/TestBinarySearchTree.java
+++ src/test/java/net/fluxcapdata/codelibrary/tree/TestBinarySearchTree.java
@@ -15,11 +15,27 @@ public class TestBinarySearchTree {
         Random random = new Random();
         BinarySearchTree<Integer> b = new BinarySearchTree<>();
         List<Integer> list = new ArrayList<>();
+        int g1a=0;
+        int g2a=0;
+        int g3a=0;
         for (int i=0; i<1000; i++) {
             int r = random.nextInt();
+            if (r == -1) { continue; }
             list.add(r);
             b.add(r);
+            g3a=g2a;
+            g2a=g1a;
+            g1a=r;
         }
+        Integer g1b = b.get(g1a);
+        Integer g2b = b.get(g2a);
+        Integer g3b = b.get(g3a);
+        Integer g4b = b.get(-1);
+
+        Assertions.assertEquals(g1a, g1b);
+        Assertions.assertEquals(g2a, g2b);
+        Assertions.assertEquals(g3a, g3b);
+        Assertions.assertNull(g4b);
 
         Collections.sort(list);
         List<Integer> blist1 = b.getInOrder();
