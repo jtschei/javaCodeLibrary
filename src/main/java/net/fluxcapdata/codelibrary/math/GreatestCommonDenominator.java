fab015c843370c96cf03cd95b70a02da793b2653
johns <johnscheibmeir@gmail.com>
2024-08-26 21:16:28 -0700

    add different methods to calculate

--- src/main/java/net/fluxcapdata/codelibrary/math/GreatestCommonDenominator.java
+++ src/main/java/net/fluxcapdata/codelibrary/math/GreatestCommonDenominator.java
@@ -1,13 +1,60 @@
 package net.fluxcapdata.codelibrary.math;
 
+import java.util.InvalidPropertiesFormatException;
+
 public class GreatestCommonDenominator {
 
+    public enum Method {
+        DIVISION,
+        SUBTRACTION,
+        RECURSION
+    }
+
     public static int greatestCommonDenominator(int a, int b) {
+        return greatestCommonDenominator(Method.DIVISION, a, b);
+    }
+
+    public static int greatestCommonDenominator(Method method, int a, int b) {
+        switch (method) {
+            case DIVISION:
+                return gcdDivide(a, b);
+            case SUBTRACTION:
+                return gcdSubtract(a, b);
+            case RECURSION:
+                return gcdRecurse(a, b);
+            default:
+                return greatestCommonDenominator(a, b);
+        }
+    }
+
+    private static int gcdDivide(int a, int b) {
         while (b != 0) {
             int t = b;
             b = a % b;
             a = t;
         }
         return Math.abs(a);
+    }
+
+    private static int gcdSubtract(int a, int b) {
+        if (a <= 0 || b <= 0) {
+            throw new RuntimeException("Inputs must be positive");
+        }
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        return Math.abs(a);
+    }
+
+    private static int gcdRecurse(int a, int b) {
+        if (b==0) {
+            return Math.abs(a);
+        } else {
+            return gcdRecurse(b, a % b);
+        }
     }
 }
--- src/test/java/net/fluxcapdata/codelibrary/math/TestGreatestCommonDenominator.java
+++ src/test/java/net/fluxcapdata/codelibrary/math/TestGreatestCommonDenominator.java
@@ -6,58 +6,139 @@ import org.junit.jupiter.api.Test;
 public class TestGreatestCommonDenominator {
 
     @Test
+    public void TestGCD0() {
+        for (GreatestCommonDenominator.Method m : GreatestCommonDenominator.Method.values()) {
+            int answer1 = GreatestCommonDenominator.greatestCommonDenominator(m, 8172, 128);
+            int answer2 = GreatestCommonDenominator.greatestCommonDenominator(m, 128, 8172);
+            Assertions.assertEquals(4, answer1);
+            Assertions.assertEquals(4, answer2);
+        }
+        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(8172, 128);
+        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(128, 8172);
+        Assertions.assertEquals(4, answer1);
+        Assertions.assertEquals(4, answer2);
+    }
+
+    @Test
     public void TestGCD1() {
-        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(100,10);
-        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(10,100);
-        Assertions.assertEquals(10,answer1);
-        Assertions.assertEquals(10,answer2);
+        for (GreatestCommonDenominator.Method m : GreatestCommonDenominator.Method.values()) {
+            int answer1 = GreatestCommonDenominator.greatestCommonDenominator(m, 100, 10);
+            int answer2 = GreatestCommonDenominator.greatestCommonDenominator(m, 10, 100);
+            Assertions.assertEquals(10, answer1);
+            Assertions.assertEquals(10, answer2);
+        }
+        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(100, 10);
+        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(10, 100);
+        Assertions.assertEquals(10, answer1);
+        Assertions.assertEquals(10, answer2);
     }
 
     @Test
     public void TestGCD2() {
-        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(100,99);
-        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(99,100);
-        Assertions.assertEquals(1,answer1);
-        Assertions.assertEquals(1,answer2);
+        for (GreatestCommonDenominator.Method m : GreatestCommonDenominator.Method.values()) {
+            int answer1 = GreatestCommonDenominator.greatestCommonDenominator(m, 100, 99);
+            int answer2 = GreatestCommonDenominator.greatestCommonDenominator(m, 99, 100);
+            Assertions.assertEquals(1, answer1);
+            Assertions.assertEquals(1, answer2);
+        }
+        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(100, 99);
+        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(99, 100);
+        Assertions.assertEquals(1, answer1);
+        Assertions.assertEquals(1, answer2);
     }
 
     @Test
     public void TestGCD3() {
-        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(4,-2);
-        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(-2,4);
-        Assertions.assertEquals(2,answer1);
-        Assertions.assertEquals(2,answer2);
+        for (GreatestCommonDenominator.Method m : GreatestCommonDenominator.Method.values()) {
+            if (m == GreatestCommonDenominator.Method.SUBTRACTION) {
+                Assertions.assertThrows(RuntimeException.class, () -> GreatestCommonDenominator.greatestCommonDenominator(m, 4, -2));
+                Assertions.assertThrows(RuntimeException.class, () -> GreatestCommonDenominator.greatestCommonDenominator(m, -2, 4));
+            } else {
+                int answer1 = GreatestCommonDenominator.greatestCommonDenominator(m, 4, -2);
+                int answer2 = GreatestCommonDenominator.greatestCommonDenominator(m, -2, 4);
+                Assertions.assertEquals(2, answer1);
+                Assertions.assertEquals(2, answer2);
+            }
+        }
+        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(4, -2);
+        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(-2, 4);
+        Assertions.assertEquals(2, answer1);
+        Assertions.assertEquals(2, answer2);
     }
 
     @Test
     public void TestGCD4() {
-        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(-4,-2);
-        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(-2,-4);
-        Assertions.assertEquals(2,answer1);
-        Assertions.assertEquals(2,answer2);
+        for (GreatestCommonDenominator.Method m : GreatestCommonDenominator.Method.values()) {
+            if (m == GreatestCommonDenominator.Method.SUBTRACTION) {
+                Assertions.assertThrows(RuntimeException.class, () -> GreatestCommonDenominator.greatestCommonDenominator(m, -4, -2));
+                Assertions.assertThrows(RuntimeException.class, () -> GreatestCommonDenominator.greatestCommonDenominator(m, -2, -4));
+            } else {
+                int answer1 = GreatestCommonDenominator.greatestCommonDenominator(m, -4, -2);
+                int answer2 = GreatestCommonDenominator.greatestCommonDenominator(m, -2, -4);
+                Assertions.assertEquals(2, answer1);
+                Assertions.assertEquals(2, answer2);
+            }
+        }
+        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(-4, -2);
+        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(-2, -4);
+        Assertions.assertEquals(2, answer1);
+        Assertions.assertEquals(2, answer2);
     }
 
     @Test
     public void TestGCD5() {
-        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(4,0);
-        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(0,4);
-        Assertions.assertEquals(4,answer1);
-        Assertions.assertEquals(4,answer2);
+        for (GreatestCommonDenominator.Method m : GreatestCommonDenominator.Method.values()) {
+            if (m == GreatestCommonDenominator.Method.SUBTRACTION) {
+                Assertions.assertThrows(RuntimeException.class, () -> GreatestCommonDenominator.greatestCommonDenominator(m, 4, 0));
+                Assertions.assertThrows(RuntimeException.class, () -> GreatestCommonDenominator.greatestCommonDenominator(m, 0, 4));
+            } else {
+                int answer1 = GreatestCommonDenominator.greatestCommonDenominator(m, 4, 0);
+                int answer2 = GreatestCommonDenominator.greatestCommonDenominator(m, 0, 4);
+                Assertions.assertEquals(4, answer1);
+                Assertions.assertEquals(4, answer2);
+            }
+        }
+        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(4, 0);
+        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(0, 4);
+        Assertions.assertEquals(4, answer1);
+        Assertions.assertEquals(4, answer2);
     }
 
     @Test
     public void TestGCD6() {
-        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(-4,0);
-        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(0,-4);
-        Assertions.assertEquals(4,answer1);
-        Assertions.assertEquals(4,answer2);
+        for (GreatestCommonDenominator.Method m : GreatestCommonDenominator.Method.values()) {
+            if (m == GreatestCommonDenominator.Method.SUBTRACTION) {
+                Assertions.assertThrows(RuntimeException.class, () -> GreatestCommonDenominator.greatestCommonDenominator(m, -4, 0));
+                Assertions.assertThrows(RuntimeException.class, () -> GreatestCommonDenominator.greatestCommonDenominator(m, 0, -4));
+            } else {
+                int answer1 = GreatestCommonDenominator.greatestCommonDenominator(m, -4, 0);
+                int answer2 = GreatestCommonDenominator.greatestCommonDenominator(m, 0, -4);
+                Assertions.assertEquals(4, answer1);
+                Assertions.assertEquals(4, answer2);
+            }
+        }
+        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(-4, 0);
+        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(0, -4);
+        Assertions.assertEquals(4, answer1);
+        Assertions.assertEquals(4, answer2);
     }
 
     @Test
     public void TestGCD7() {
-        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(Integer.MAX_VALUE,Integer.MIN_VALUE);
-        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(Integer.MIN_VALUE,Integer.MAX_VALUE);
-        Assertions.assertEquals(1,answer1);
-        Assertions.assertEquals(1,answer2);
+        for (GreatestCommonDenominator.Method m : GreatestCommonDenominator.Method.values()) {
+            if (m == GreatestCommonDenominator.Method.SUBTRACTION) {
+                Assertions.assertThrows(RuntimeException.class, () -> GreatestCommonDenominator.greatestCommonDenominator(m, Integer.MAX_VALUE, Integer.MIN_VALUE));
+                Assertions.assertThrows(RuntimeException.class, () -> GreatestCommonDenominator.greatestCommonDenominator(m, Integer.MIN_VALUE, Integer.MAX_VALUE));
+            } else {
+                int answer1 = GreatestCommonDenominator.greatestCommonDenominator(m, Integer.MAX_VALUE, Integer.MIN_VALUE);
+                int answer2 = GreatestCommonDenominator.greatestCommonDenominator(m, Integer.MIN_VALUE, Integer.MAX_VALUE);
+                Assertions.assertEquals(1, answer1);
+                Assertions.assertEquals(1, answer2);
+            }
+        }
+        int answer1 = GreatestCommonDenominator.greatestCommonDenominator(Integer.MAX_VALUE, Integer.MIN_VALUE);
+        int answer2 = GreatestCommonDenominator.greatestCommonDenominator(Integer.MIN_VALUE, Integer.MAX_VALUE);
+        Assertions.assertEquals(1, answer1);
+        Assertions.assertEquals(1, answer2);
     }
 }
